8aa0edbf3e9ca9b11ce55d2b3e031862b2deef6a
Lourenço Q <lourenco.gaspar.dq@gmail.com>
2021-01-15 01:24:56 +0100

    Template de Inserção, git ignore

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.lista_eventos),
+    path('agenda/evento/', views.evento, name='new_evento'),
+    path('agenda/evento/submit', views.submit_evento, name='submit_evento'),
+    path('agenda/', views.lista_eventos),
+    path('login/', views.login_user),
+    path('logout/', views.logout_user),
+    path('login/submit', views.submit_login)
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth  import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def login_user(request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        passsword = request.POST.get('password')
+        usuario = authenticate(username=username, password=passsword)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido")
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    dados = {'evento':Evento.objects.filter(usuario=usuario)}
+    return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+
+    return redirect('/')
